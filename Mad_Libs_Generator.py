98a45b6604e1fa621b789b6625264fdd8fe9f60b
Ogu the baby <43885005+jm-ss@users.noreply.github.com>
2021-01-14 15:48:58 +0900

    Add files via upload

---
+++ Mad_Libs_Generator.py
@@ -0,0 +1,6 @@
+color= input("enter a color")
+pluralNoun = input("enter a plural noun : ")
+celebrity = input("enter a celebrity : ")
+print("Roses are ", color)
+print(pluralNoun+ " are blue")
+print("i know im meant to be with ", celebrity)
