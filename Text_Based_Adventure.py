884e90dc2560bd9c3acbb00ecc2f762d97fc6f88
Ogu the baby <43885005+jm-ss@users.noreply.github.com>
2021-01-26 23:22:09 +0900

    Add files via upload

---
+++ Text_Based_Adventure.py
@@ -0,0 +1,11 @@
+name = str(input("Enter ur name \n"))
+print(f"{name} you are stuck in a forest. ur task is to get from the forest without dieing")
+print("u r walking threw forest and suddenlya wolf comes in ur wa. Now you have two options")
+print("1. Run 2. Climb the nearest tree")
+user= int(input("choose one optinon"))
+if user ==1:
+    print("u died")
+elif user ==2:
+    print("u died")
+else:
+    print("jerry saved you")
